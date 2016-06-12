11d93f38cdd76909f1af73c31b24776b0a5efbce
frio <fantasycool@126.com>
2016-06-12 17:11:53 +0800

    first commit

---
+++ src/main/java/com/frio/App.java
@@ -0,0 +1,13 @@
+package com.frio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
