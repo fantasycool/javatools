c775a33110abd3c9ee3627ece564f566aad9ccdb
frio <haofu@91jkys.com>
2017-02-22 16:15:39 +0800

    add exception format util

---
+++ src/main/java/com/frio/tools/exception/ExceptionPrinter.java
@@ -0,0 +1,26 @@
+package com.frio.tools.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Created by frio on 17/2/22.
+ */
+public class ExceptionPrinter {
+
+    /**
+     * get exception stack format string
+     * @param t
+     * @return
+     */
+    public static String getExceptionMessage(Throwable t){
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        t.printStackTrace(printWriter);
+        return stringWriter.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getExceptionMessage(new RuntimeException()));
+    }
+}
