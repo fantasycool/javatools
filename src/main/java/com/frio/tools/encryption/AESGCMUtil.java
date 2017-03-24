328defbfc4b25453fa885811a834b126d92343b2
frio <haofu@91jkys.com>
2017-03-24 23:23:54 +0800

    update

--- src/main/java/com/frio/tools/encryption/AESGCMUtil.java
+++ src/main/java/com/frio/tools/encryption/AESGCMUtil.java
@@ -2,11 +2,13 @@ package com.frio.tools.encryption;
 
 import org.apache.commons.codec.binary.Base64;
 
-import javax.crypto.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.*;
-import java.util.Arrays;
 
 /**
  * Symetrical Entryption
@@ -16,9 +18,6 @@ import java.util.Arrays;
  * Created by frio on 17/3/23.
  */
 public class AESGCMUtil {
-    public static final int AES_KEY_SIZE = 128;
-    public static final int GCM_NONCE_LENGTH = 12;
-    public static final int GCM_TAG_LENGTH = 16;
     public static final String PROVIDER = "SunJCE";
     public static final String ALGORITHM = "AES/GCM/NoPadding";
 
@@ -39,7 +38,7 @@ public class AESGCMUtil {
             GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(16 * 8, iv);
             SecretKeySpec secretKeySpec = new SecretKeySpec(keys, "AES");
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, gcmParameterSpec);
-            cipher.updateAAD(Base64.decodeBase64(aad));
+            cipher.updateAAD(aad.getBytes());
             byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
             byte[] resultData = new byte[encryptedBytes.length + 16];
             System.arraycopy(encryptedBytes, 0, resultData, 0, encryptedBytes.length);
@@ -64,7 +63,7 @@ public class AESGCMUtil {
             SecretKeySpec secretKeySpec = new SecretKeySpec(keys, "AES");
             Cipher cipher = Cipher.getInstance(ALGORITHM, PROVIDER);
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, gcmParameterSpec);
-            cipher.update(aad.getBytes());
+            cipher.updateAAD(aad.getBytes());
             return new String(cipher.doFinal(encryptedBytes, 0, encryptedBytes.length - 16));
         }catch(Exception e){
             throw new RuntimeException(e);
@@ -72,62 +71,9 @@ public class AESGCMUtil {
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        int testNum = 0;
-
-        byte[] input = "Hello AES_GCM World".getBytes();
-
-        SecureRandom random = SecureRandom.getInstanceStrong();
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(AES_KEY_SIZE, random);
-        SecretKey key = keyGen.generateKey();
-        System.out.println(key.getEncoded().length); // length is 16;
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE");
-
-        final byte[] nonce = new byte[GCM_NONCE_LENGTH];
-        random.nextBytes(nonce);
-        GCMParameterSpec spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce);
-        System.out.println("tLength is:" + spec.getTLen());
-        cipher.init(Cipher.ENCRYPT_MODE, key, spec);
-
-        byte[] aad = "Whatever I like".getBytes();
-        cipher.updateAAD(aad);
-        byte[] cipherText = cipher.doFinal(input);
-
-
-        Cipher cipher1 = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE");
-        cipher1.init(Cipher.DECRYPT_MODE, key, spec);
-
-//        if(testNum == 1){
-//            aad[1] ++;
-//        }
-        cipher1.updateAAD("Whatever I like".getBytes());
-        if (testNum == 2) {
-            cipherText[10]++;
-        }
-        if (testNum == 3) {
-            cipherText[cipherText.length - 2]++;
-        }
-        try {
-            byte[] plainText = cipher1.doFinal(cipherText);
-            String text = new String(plainText);
-            if (testNum != 0) {
-                System.out.println("Test Failed: expected  AEADBadTagException not thrown!");
-            } else {
-                if (Arrays.equals(input, plainText)) {
-                    System.out.println("Test passed, match!");
-                } else {
-                    System.out.println("Test failed: result mismatch!");
-                    System.out.println(new String(plainText));
-                }
-            }
-        } catch (AEADBadTagException e) {
-            if (testNum == 0) {
-                System.out.println("Test failed: unexpected ex" + e);
-                e.printStackTrace();
-            } else {
-                System.out.println("Test passed!expected ex" + e);
-            }
-        }
-
+        String privateKey = SymmetricalEncryptionUtil.generatePrivateKeyFor(128, "AES");
+        String encryptedStr = encryptData("sadfsafads", Base64.decodeBase64(privateKey), "abc");
+        System.out.println(encryptedStr);
+        System.out.println(decryptData(encryptedStr, Base64.decodeBase64(privateKey), "abc"));
     }
 }
