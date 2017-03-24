eb3bfa84a0e001d4e87ccbde69bc5ab53e4ea2ed
frio <haofu@91jkys.com>
2017-03-24 13:41:52 +0800

    update symmetrical encryption

--- src/main/java/com/frio/tools/encryption/AESGCMUtil.java
+++ src/main/java/com/frio/tools/encryption/AESGCMUtil.java
@@ -3,6 +3,7 @@ package com.frio.tools.encryption;
 import javax.crypto.*;
 import javax.crypto.spec.GCMParameterSpec;
 import java.security.*;
+import java.util.Arrays;
 
 /**
  * Symetrical Entryption
@@ -18,23 +19,21 @@ public class AESGCMUtil {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         int testNum = 0;
-        if (args.length >= 0) {
-            testNum = Integer.parseInt(args[0]);
-            if (testNum < 0 || testNum > 3) {
-                System.exit(-1);
-            }
-        }
+
         byte[] input = "Hello AES_GCM World".getBytes();
         SecureRandom random = SecureRandom.getInstanceStrong();
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         keyGen.init(AES_KEY_SIZE, random);
         SecretKey key = keyGen.generateKey();
-
+        System.out.println(key.getEncoded().length); // length is 16;
         Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "SunJCE");
+
         final byte[] nonce = new byte[GCM_NONCE_LENGTH];
         random.nextBytes(nonce);
         GCMParameterSpec spec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce);
+        System.out.println("tLength is:" + spec.getTLen());
         cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+
         byte[] aad = "Whatever I like".getBytes();
         cipher.updateAAD(aad);
 
@@ -44,7 +43,33 @@ public class AESGCMUtil {
         if(testNum == 1){
             aad[1] ++;
         }
-
+        cipher.updateAAD(aad);
+        if(testNum == 2){
+            cipherText[10] ++;
+        }
+        if(testNum == 3){
+            cipherText[cipherText.length - 2] ++;
+        }
+        try{
+            byte[] plainText = cipher.doFinal(cipherText);
+            if(testNum != 0){
+                System.out.println("Test Failed: expected  AEADBadTagException not thrown!");
+            }else{
+                if(Arrays.equals(input, plainText)){
+                    System.out.println("Test passed, match!");
+                }else{
+                    System.out.println("Test failed: result mismatch!");
+                    System.out.println(new String(plainText));
+                }
+            }
+        }catch(AEADBadTagException e){
+            if(testNum == 0){
+                System.out.println("Test failed: unexpected ex" + e);
+                e.printStackTrace();
+            }else{
+                System.out.println("Test passed!expected ex" + e);
+            }
+        }
 
     }
 }
---
+++ src/main/java/com/frio/tools/encryption/SymmetricalEncryptionUtil.java
@@ -0,0 +1,134 @@
+package com.frio.tools.encryption;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+
+
+/**
+ * Symmetrical encryption need two things to encrypt messages
+ * 1. private key
+ * 2. IV
+ * Because when symmetrical encryption is doing encryption, it cuts message to a lot of block.
+ * then it use one block to XOR it's previous entrypted block messsage to get a new entrypted block.
+ * But the problem is the first block doesn't have a previous block, so we have to create 0 the block
+ * for the first block.
+ * We have to make sure every request message use the different start IV.
+ * The dangerous point is, for example:
+ * AAAAAA
+ * BBBBBB
+ * <p>
+ * AAAAAA
+ * CCCCCC
+ * <p>
+ * These two messages will have the same head encrypted data.
+ * So, we have to generate differcent iv parameters every time we encrypt different message request.
+ * <p>
+ * But, how does the others know what the iv is ?
+ * The method is, we append the iv bytes to the encryted result, and when the receiver got a encrypted message,
+ * he can cut off the last 16 num bytes as the iv parameters.
+ */
+public class SymmetricalEncryptionUtil {
+    public final static String DEFAULT_ALGORITHM = "AES/CBC/PKCS5PADDING";
+
+    /**
+     * We default use AES/CBC/PKCS5PADDING algorithm
+     * default use 16 length IV
+     * 16 length iv bytes is append to the last of encrypted data,
+     * please subtract it first.
+     *
+     * @param plainText
+     * @param keys
+     * @return
+     * @throws Exception
+     */
+    public static String encryptData(String plainText, byte[] keys, String algorithm) {
+        if (StringUtils.isBlank(algorithm)) {
+            algorithm = DEFAULT_ALGORITHM;
+        }
+        try {
+            // generate iv
+            SecureRandom random = SecureRandom.getInstanceStrong();
+            byte[] iv = new byte[16];
+            random.nextBytes(iv);
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            // init cipher
+            Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, DEFAULT_ALGORITHM, keys, ivParameterSpec);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+            byte[] resultData = new byte[encryptedBytes.length + 16];
+            System.arraycopy(encryptedBytes, 0, resultData, 0, encryptedBytes.length);
+            System.arraycopy(ivParameterSpec.getIV(), 0, resultData, encryptedBytes.length, ivParameterSpec.getIV().length);
+            return Base64.encodeBase64String(resultData);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * We default use AES/CBC/PKCS5PADDING algorithm
+     * default use 16 length IV
+     * 16 length iv bytes is append to the last of encrypted data,
+     * please subtract it first.
+     *
+     * @param encrypted
+     * @param key
+     * @param algorithm
+     * @return
+     * @throws Exception
+     */
+    public static String decryptData(String encrypted, byte[] key, String algorithm) throws Exception {
+        if (StringUtils.isBlank(algorithm)) {
+            algorithm = DEFAULT_ALGORITHM;
+        }
+        byte[] encryptedBytes = Base64.decodeBase64(encrypted);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(encryptedBytes, encryptedBytes.length - 16, 16);
+        Cipher cipher = getCipher(Cipher.DECRYPT_MODE, DEFAULT_ALGORITHM, key, ivParameterSpec);
+        return new String(cipher.doFinal(encryptedBytes, 0, encryptedBytes.length - 16));
+    }
+
+    private static Cipher getCipher(int cipherMode, String encryptionAlgorithm,
+                                    byte[] key, IvParameterSpec ivParameterSpec)
+            throws Exception {
+        String algorithmType = encryptionAlgorithm.split("/")[0];
+        SecretKeySpec keySpecification = new SecretKeySpec(
+                key, algorithmType);
+        Cipher cipher = Cipher.getInstance(DEFAULT_ALGORITHM);
+        cipher.init(cipherMode, keySpecification, ivParameterSpec);
+        return cipher;
+    }
+
+    /**
+     * we use this method to generate a private key for use
+     * @param length
+     * @return
+     */
+    public static String generatePrivateKeyFor(int length, String algorithm){
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
+            SecureRandom secureRandom = SecureRandom.getInstanceStrong();
+            keyGen.init(length, secureRandom);
+            SecretKey secretKey = keyGen.generateKey();
+            return Base64.encodeBase64String(secretKey.getEncoded());
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args){
+        try {
+            String privateKey = generatePrivateKeyFor(128, "AES");
+            String encryptedResult = encryptData("这是一个HelloWorld,哈哈!", Base64.decodeBase64(privateKey), DEFAULT_ALGORITHM);
+            System.out.println(encryptedResult);
+            System.out.println(decryptData(encryptedResult, Base64.decodeBase64(privateKey), DEFAULT_ALGORITHM));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
